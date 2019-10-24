aa5d6c7b69cf5e24949118d6b52bbc39efc7e60d
Cloud1103 <abg007889@yahoo.com.hk>
2019-10-24 14:51:28 +0800

    SetAudioMixer

---
+++ UI/Assets/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Audio; // using Audio API(功能)
+
+public class GameManager : MonoBehaviour {
+    public AudioMixer mixer;
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+}
