1f8eb04c47c250a19cff00904b46f9dd3e2e39cf
Cloud1103 <abg007889@yahoo.com.hk>
2019-11-14 14:50:44 +0800

    Version1.0.0

--- UI/Assets/GameManager.cs
+++ UI/Assets/GameManager.cs
@@ -2,10 +2,11 @@
 using UnityEngine.Audio; // using Audio API(功能)
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameManager : MonoBehaviour {
     public AudioMixer mixer;
-    public Text loadPersent;
+    public Text loadingText;
     public Slider loading;
     public void SetVBGM(float value)
     {
@@ -17,6 +18,31 @@ public class GameManager : MonoBehaviour {
     }
     public void Play()
     {
-        SceneManager.LoadScene("Game_Demo");
+        //SceneManager.LoadScene("Game_Demo");
+        StartCoroutine(Loading());
+    }
+
+    //協同程序 Coroutine
+    private IEnumerator Loading()
+    {
+        //print("test 1");
+        //yield return new WaitForSeconds(1);
+        //print("test 2");
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync("Game_Demo");
+        ao.allowSceneActivation = false;
+
+        while(ao.isDone == false)
+        {
+            loadingText.text = ((ao.progress / 0.9f) * 100).ToString();
+            loading.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.0001f);
+
+            if(ao.progress == 0.9f && Input.anyKey)
+            {
+                ao.allowSceneActivation = true;
+            }
+        }
+
     }
 }
