01e9d60abfe4602154c7171a765adf956097859a
Cloud1103 <abg007889@yahoo.com.hk>
2019-12-05 14:50:28 +0800

    PauseSetting

---
+++ UI/Assets/PauseControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour {
+
+    [Header("StopPage")]
+    public Image imagePause;
+    public Sprite spritePause, spritePlay;
+    [Header("Stop")]
+    public bool pause;
+
+    private void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("Stop~");
+            pause = !pause;
+            imagePause.sprite = pause ? spritePlay : spritePause;
+            Time.timeScale = pause ? 0 : 1;
+        }
+    }
+
+    private void Update()
+    {
+        Pause();
+    }
+
+}
