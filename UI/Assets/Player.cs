f963d1c33e4dcf13ee097441ee12d2720ef6fc64
Cloud1103 <abg007889@yahoo.com.hk>
2019-12-12 14:54:53 +0800

    Trap

---
+++ UI/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int hp = 100;
+    public Slider hpSlider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+        if(other.tag == "Trap")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print(other.gameObject);
+
+        if (other.tag == "Trap")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+    }
+}
---
+++ UI/Assets/Trap.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+    [Header("Demage"), Range(1, 50)]
+    public int damage = 10;
+}
